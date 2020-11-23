78dd353792a8ce6c50f2852bae8537223a04266b
Neon-Fox <69065710+Neon-Fox@users.noreply.github.com>
2020-11-23 18:03:56 +1100

    NeonSploit Logo Animation
    This is the official logo animation for NeonSploit.

---
+++ LogoAnimation.lua
@@ -0,0 +1,30 @@
+-- NeonSploit Logo Animation
+local blur = Instance.new("BlurEffect", game.Lighting)
+blur.Size = 0
+local ScreenGui = Instance.new("ScreenGui")
+local ImageLabel = Instance.new("ImageLabel")
+ScreenGui.Parent = game.CoreGui
+ImageLabel.Parent = ScreenGui
+ImageLabel.BackgroundColor3 = Color3.new(1, 1, 1)
+ImageLabel.BackgroundTransparency = 1
+ImageLabel.Position = UDim2.new(0.5, -(432 / 2), 0.5, -(135 / 2))
+ImageLabel.Rotation = 0
+ImageLabel.Size = UDim2.new(0, 432, 0, 135)
+ImageLabel.Image = "rbxassetid://5653513783"
+ImageLabel.ImageTransparency = 1
+setclipboard("https://www.neonhax.tk")
+for i = 1, 50, 2 do
+    blur.Size = i
+    ImageLabel.ImageTransparency = ImageLabel.ImageTransparency - 0.1
+    wait()
+end
+wait(.25) -- How many seconds you want it to display the logo.
+ 
+for i = 1, 50, 2 do
+    blur.Size = 50 - i
+    ImageLabel.ImageTransparency = ImageLabel.ImageTransparency + 0.1
+    wait()
+end
+blur:Destroy()
+ScreenGui:Destroy()
+wait(0)
